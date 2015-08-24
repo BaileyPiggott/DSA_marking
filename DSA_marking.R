224b26e1af3a99e24c2de263e67992cdd2253ba8
Bailey <baileypiggott@gmail.com>
2015-08-24 16:00:17 -0400

    Include phrases in text analysis

--- DSA_marking.R
+++ DSA_marking.R
@@ -5,39 +5,41 @@ library(dplyr)
 library(magrittr)
 library(NLP) # required for tm package
 library(tm) 
+library(RWeka)
 
 source("function_word_frequency.R")
+source("function_phrase_freq.R")
 
 # read text and count words ----------------
 dsa_1101 <- read.table("DSA_samples/DSA_1101.txt", sep = "@") # pick separation character as something that will not appear in the text
-wf_1101 <- word_freq(dsa_1101, "1101")
+wf_1101 <- rbind(word_freq(dsa_1101, "1101"), ngram_freq(dsa_1101, "1101"))
 
-dsa_1146 <- read.table("DSA_samples/DSA_1146.txt", sep = "@") # pick separation character as something that will not appear in the text
-wf_1146 <- word_freq(dsa_1146, "1146")
+dsa_1146 <- read.table("DSA_samples/DSA_1146.txt", sep = "@") 
+wf_1146 <- rbind(word_freq(dsa_1146, "1146"), ngram_freq(dsa_1146, "1146"))
 
-dsa_1552 <- read.table("DSA_samples/DSA_1552.txt", sep = "@") # pick separation character as something that will not appear in the text
-wf_1552 <- word_freq(dsa_1552, "1552")
+dsa_1552 <- read.table("DSA_samples/DSA_1552.txt", sep = "@") 
+wf_1552 <- rbind(word_freq(dsa_1552, "1552"), ngram_freq(dsa_1552, "1552"))
 
-dsa_1666 <- read.table("DSA_samples/DSA_1666.txt", sep = "@") # pick separation character as something that will not appear in the text
-wf_1666 <- word_freq(dsa_1666, "1666")
+dsa_1666 <- read.table("DSA_samples/DSA_1666.txt", sep = "@") 
+wf_1666 <- rbind(word_freq(dsa_1666, "1666"), ngram_freq(dsa_1666, "1666"))
 
-dsa_1338 <- read.table("DSA_samples/DSA_1338.txt", sep = "@") # pick separation character as something that will not appear in the text
-wf_1338 <- word_freq(dsa_1338, "1338")
+dsa_1338 <- read.table("DSA_samples/DSA_1338.txt", sep = "@") 
+wf_1338 <- rbind(word_freq(dsa_1338, "1338"), ngram_freq(dsa_1338, "1338"))
 
-dsa_1773 <- read.table("DSA_samples/DSA_1773.txt", sep = "@") # pick separation character as something that will not appear in the text
-wf_1773 <- word_freq(dsa_1773, "1773")
+dsa_1773 <- read.table("DSA_samples/DSA_1773.txt", sep = "@") 
+wf_1773 <- rbind(word_freq(dsa_1773, "1773"), ngram_freq(dsa_1773, "1773"))
 
-dsa_1381 <- read.table("DSA_samples/DSA_1381.txt", sep = "@") # pick separation character as something that will not appear in the text
-wf_1381 <- word_freq(dsa_1381, "1381")
+dsa_1381 <- read.table("DSA_samples/DSA_1381.txt", sep = "@") 
+wf_1381 <- rbind(word_freq(dsa_1381, "1381"), ngram_freq(dsa_1381, "1381"))
 
-dsa_1834 <- read.table("DSA_samples/DSA_1834.txt", sep = "@") # pick separation character as something that will not appear in the text
-wf_1834 <- word_freq(dsa_1834, "1834")
+dsa_1834 <- read.table("DSA_samples/DSA_1834.txt", sep = "@") 
+wf_1834 <- rbind(word_freq(dsa_1834, "1834"), ngram_freq(dsa_1834, "1834"))
 
-dsa_1258 <- read.table("DSA_samples/DSA_1258.txt", sep = "@") # pick separation character as something that will not appear in the text
-wf_1258 <- word_freq(dsa_1258, "1258")
+dsa_1258 <- read.table("DSA_samples/DSA_1258.txt", sep = "@") 
+wf_1258 <- rbind(word_freq(dsa_1258, "1258"), ngram_freq(dsa_1258, "1258"))
 
-dsa_1892 <- read.table("DSA_samples/DSA_1892.txt", sep = "@") # pick separation character as something that will not appear in the text
-wf_1892 <- word_freq(dsa_1892, "1892")
+dsa_1892 <- read.table("DSA_samples/DSA_1892.txt", sep = "@") 
+wf_1892 <- rbind(word_freq(dsa_1892, "1892"), ngram_freq(dsa_1892, "1892"))
 
 
 # combine into one data frame -----------------------------------
@@ -57,8 +59,7 @@ all_samples <- wf_1101 %>%
 rownames(all_samples)<- all_samples[,1 ] #convert 'word' column to row names
 all_samples <- all_samples[,-1] #remove original 'word' column
 
-all_samples <- data.frame(t(all_samples)) %>% #transpose and convert to data frame
- mutate(ID = rownames(.))
+all_samples <- data.frame(t(all_samples))
 
 #filter out words that only appear in one sample --------------------------------
 
@@ -68,6 +69,8 @@ all_samples <- Filter( # remove columns where only one report contains that word
   ), 
   all_samples)
 
+all_samples <- all_samples %>% mutate(ID = row.names(.)) #convert ID's into an actual column for merging
+
 # bind with scores ------------------------
 
 DSA_scores <- read.csv("DSA Scores.csv") %>% select(ID, PD, CD, Pre_Design, DD, Validation, Implementation, Process)
@@ -75,13 +78,12 @@ DSA_scores <- read.csv("DSA Scores.csv") %>% select(ID, PD, CD, Pre_Design, DD, 
 DSA_scores$ID <- as.numeric(DSA_scores$ID)
 all_samples$ID <- as.numeric(all_samples$ID)
 
-DSA_data <- full_join(DSA_scores, all_samples, by = "ID") # join so scores and text analysis are in the same order
+DSA_data <- full_join(DSA_scores, all_samples, by = "ID") # join so scores and text analysis are in the same order by ID
 
 DSA_scores <- DSA_data %>% select(PD:Process) # columns of scores
 DSA_text <- DSA_data %>% select(-c(ID:Process)) # columns of text analysis data
 
 # export for analysis in matlab ------------------
 
-write.csv(DSA_scores, file ="DSA_scores.csv")
-
-write.csv(DSA_text, file ="DSA_text.csv")
+write.csv(DSA_scores, file ="DSA_scores.csv") # csv of scores
+write.csv(DSA_text, file ="DSA_text.csv") # csv of text analysis
